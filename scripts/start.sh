b370d0973d23164442b09ae355927eabe28bcff5
Prakasa <prakasa@devetek.com>
2026-03-19 12:02:12 +0700

    fix: use default env OPENCLAW_GATEWAY_TOKEN

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ export OPENCLAW_HOME="./.openclaw"
 export OPENCLAW_STATE_DIR="$OPENCLAW_HOME/state"
 export OPENCLAW_CONFIG_PATH="config.json"
 
-export OPENCLAW_TOKEN="YOUR-OPENCLAW-TOKEN-HERE"
+export OPENCLAW_GATEWAY_TOKEN="YOUR-OPENCLAW-TOKEN-HERE"
 export OPENCLAW_TELEGRAM_TOKEN="YOUR-TELEGRAM-TOKEN-HERE"
 
-./node_modules/.bin/openclaw gateway --port ${PORT} --allow-unconfigured --bind auto --auth token --token "${OPENCLAW_TOKEN}"
+./node_modules/.bin/openclaw gateway --port ${PORT} --allow-unconfigured --bind auto --auth token --token "${OPENCLAW_GATEWAY_TOKEN}"
